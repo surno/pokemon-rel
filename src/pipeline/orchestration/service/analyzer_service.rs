4f91db4fe4b3a5c918538940570424cee16f062b
Antonio Ontiveros <antonioontiveros93@gmail.com>
2025-10-30 21:47:43 -0700

    Adds unit test for analyzer service

--- src/pipeline/orchestration/service/analyzer_service.rs
+++ src/pipeline/orchestration/service/analyzer_service.rs
@@ -1,7 +1,6 @@
 use std::pin::Pin;
 use std::sync::Arc;
 
-use crate::error::AppError;
 use crate::pipeline::context::frame_context::FrameContext;
 use crate::pipeline::context::state::AnalyzedState;
 use crate::pipeline::context::state::IngestedState;
@@ -42,5 +41,38 @@ impl Service<FrameContext<IngestedState>> for AnalyzerService {
         });
 
         future
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use chrono::Utc;
+    use image::{DynamicImage, ImageBuffer, Rgb};
+    use uuid::Uuid;
+
+    use crate::{
+        common::Frame,
+        pipeline::{
+            domain::scene_analysis::SceneType, orchestration::step::scene_analyzer::SceneAnalyzer,
+        },
+    };
+
+    use super::*;
+
+    #[tokio::test]
+    async fn test_analyzer_service() {
+        let mut analyzer_service = AnalyzerService::new(Box::new(SceneAnalyzer::new()));
+        let frame_context = FrameContext::new(Frame::new(
+            Uuid::new_v4(),
+            DynamicImage::ImageRgb8(ImageBuffer::<Rgb<u8>, Vec<u8>>::from_pixel(
+                100,
+                100,
+                Rgb([255, 255, 255]),
+            )),
+            Utc::now(),
+            Uuid::new_v4(),
+        ));
+        let response = analyzer_service.call(frame_context).await.unwrap();
+        assert!(response.analysis().scene_type() == SceneType::Unknown);
     }
 }
